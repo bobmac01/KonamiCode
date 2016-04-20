5660a6f68985e50bc46348118527446cb19f678d
bobmac <bob@robertmcateer.com>
2016-04-20 09:54:28 +0100

    Initial commit

---
+++ js/konamicode.js
@@ -0,0 +1,42 @@
+/**
+ * Created by bobmac on 20/04/2016.
+ */
+
+var allowed = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
+    65: 'a',
+    66: 'b'
+};
+
+// The actual keys needed to activate the konami code
+var code = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'a', 'b'];
+
+// counter to check the position of key presses
+var position = 0;
+
+document.addEventListener('keydown', function(e) {
+    // gets the values for the specified key
+    var key = allowed[e.keyCode];
+   // gets value of the requried key from the code array
+    var requiredKey = code[position];
+
+
+    if (key == requiredKey) {
+
+        // add one position to the position counter
+        position++;
+
+        // final key is pushed, function DoSomething() will run
+        if (position == code.length)
+            DoSomething();
+    } else
+        position = 0;
+});
+
+function DoSomething() {
+    document.body.style.backgroundImage = "url('images/paganiHBC.jpg')";
+    alert("cheats activated");
+}
